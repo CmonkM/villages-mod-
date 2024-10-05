2aa9b06b8ad72dffd855adf03860f33d986f9964
joe <joe@admins-MacBook-Air-3.local>
2024-10-05 20:45:47 +0100

    Initial commit

---
+++ src/main/java/net/cmonk/villagesmod/VillagesModClient.java
@@ -0,0 +1,10 @@
+package net.cmonk.villagesmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VillagesModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
